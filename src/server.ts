22d6b1d68f8420442d51a5d92fafd5c1905dcda2
genesis-kim <genesis.kim@navercorp.com>
2021-06-14 18:07:48 +0900

    Convert TS and 1.3 POC API

---
+++ src/server.ts
@@ -0,0 +1,41 @@
+import { ApolloServer, gql } from 'apollo-server';
+
+const typeDefs = gql`
+  type Movie {
+    title: String
+    year: Int
+  }
+  type Query {
+     movies: [Movie]
+     movie: Movie
+  }
+  type Mutation {
+    createMovie(title: String!): Boolean
+    deleteMovie(title: String!): Boolean
+  }
+`;
+
+const resolvers = {
+  Query: {
+    movies: () => [],
+    movie: () => ({"title": "Hello", year: 2021}),
+  },
+  Mutation: {
+    createMovie: (_, args) => {
+      console.log(args);
+      return true;
+    },
+    deleteMovie: (_, args) => {
+      console.log(args);
+      return true;
+    },
+  },
+};
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+server
+  .listen()
+  .then(() => console.log('server is running on https://localhost:4000/'))
